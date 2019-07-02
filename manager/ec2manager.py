d205bacaef9f5ae56eb0c7dc6daac71a6c144133
Walter Kramer <walter.kramer@inqdo.com>
2019-07-02 13:06:02 +0200

    Update script to use Click module, and show more information about instances

--- manager/ec2manager.py
+++ manager/ec2manager.py
@@ -1,8 +1,21 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='ec2manager')
+ec2 = session.resource('ec2')
+
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='ec2manager')
-    ec2 = session.resource('ec2')
+    list_instances()
 
-    for i in ec2.instances.all():
-        print(i)
+
